003dcc8e2e6120166815d8976a341f02c37033e0
uwan95 <42994316+uwan95@users.noreply.github.com>
2018-09-06 01:25:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,12 @@
+<!DOCTYPE html>
 <html>
-   <head>
-      <title>Theme Ujicoba</title>
-   </head>
-   <body>
-      <div id="wrap">
-       <div id="header">
-        Lokasi header ada disini
-     </div>
-     <div id="maincontent">
-        <div id="content">
-         Artikel akan muncul disini
-      </div>
-      <div id="sidebar">
-         <div id="lebar">
-            Iklan kotak ada disini
-         </div>
-         <div id="kiri">
-            Menu-menu sidebar kiri ada disini
-         </div>
-         <div id="kanan">
-            Menu-menu sidebar kanan ada disini
-         </div>
-      </div>
-     </div>
-     <div style="clear:both;"></div>
-     <div id="footer">
-        Footer ada disini
-     </div>
-    </div>
-   </body>
+<head>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<title>Page Title</title>
+</head>
+<body>
+
+
+</body>
 </html>
