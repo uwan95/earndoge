3f55375e74e4d05d44488d221376631b2142c070
uwan95 <42994316+uwan95@users.noreply.github.com>
2018-09-05 14:26:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
 <html>
    <head>
-      <title>Judul Blog</title>
+      <title>Theme Ujicoba</title>
    </head>
    <body>
-      <h1>Judul Artikel</h1>
-      <p>Artikel yang ingin dimunculkan</p>
+      <div id="wrap">
+       <div id="header">
+        Lokasi header ada disini
+     </div>
+     <div id="maincontent">
+        <div id="content">
+         Artikel akan muncul disini
+      </div>
+      <div id="sidebar">
+         <div id="lebar">
+            Iklan kotak ada disini
+         </div>
+         <div id="kiri">
+            Menu-menu sidebar kiri ada disini
+         </div>
+         <div id="kanan">
+            Menu-menu sidebar kanan ada disini
+         </div>
+      </div>
+     </div>
+     <div style="clear:both;"></div>
+     <div id="footer">
+        Footer ada disini
+     </div>
+    </div>
    </body>
 </html>
